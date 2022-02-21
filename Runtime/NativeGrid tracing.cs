ee1dff1c11e3528966303408a672b7cced63c9a7
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2022-02-21 23:27:42 +0100

    TraceLine ( INT2 A , INT2 B , NativeList<int2> results )// no min-max variant

--- Runtime/NativeGrid tracing.cs
+++ Runtime/NativeGrid tracing.cs
@@ -11,6 +11,92 @@ namespace NativeGridNamespace
 
 
 		/// <summary> Bresenham's line drawing algorithm (https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm). </summary>
+		public static void TraceLine ( INT2 A , INT2 B , NativeList<int2> results )
+		{
+			results.Clear();
+			{
+				int2 dir = math.abs( A - B );
+				int capacity = math.max( dir.x , dir.y );
+				if( results.Capacity<capacity )
+				{
+					results.Capacity = capacity;
+				}
+			}
+
+			int2 coord = A;
+			int d, dx, dy, ai, bi, xi, yi;
+
+			if( A.x<B.x )
+			{
+				xi = 1;
+				dx = B.x - A.x;
+			}
+			else
+			{
+				xi = -1;
+				dx = A.x - B.x;
+			}
+			
+			if( A.y<B.y )
+			{
+				yi = 1;
+				dy = B.y - A.y;
+			}
+			else
+			{
+				yi = -1;
+				dy = A.y - B.y;
+			}
+			
+			results.Add( coord );
+			
+			if( dx>dy )
+			{
+				ai = (dy - dx) * 2;
+				bi = dy * 2;
+				d = bi - dx;
+
+				while( coord.x!=B.x )
+				{
+					if( d>=0 )
+					{
+						coord.x += xi;
+						coord.y += yi;
+						d += ai;
+					}
+					else
+					{
+						d += bi;
+						coord.x += xi;
+					}
+
+					results.Add( coord );
+				}
+			}
+			else
+			{
+				ai = ( dx - dy ) * 2;
+				bi = dx * 2;
+				d = bi - dy;
+				
+				while( coord.y!=B.y )
+				{
+					if( d>=0 )
+					{
+						coord.x += xi;
+						coord.y += yi;
+						d += ai;
+					}
+					else
+					{
+						d += bi;
+						coord.y += yi;
+					}
+					
+					results.Add( coord );
+				}
+			}
+		}
 		public static void TraceLine ( INT2 A , INT2 B , NativeList<int2> results , int2 min , int2 max )
 		{
 			results.Clear();
